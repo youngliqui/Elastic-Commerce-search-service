de05d664d473c09dc2f9422349cf52c883a4873a
youngliqui <super.pav-79@yandex.by>
2025-10-18 21:41:09 +0300

    init

---
+++ src/main/kotlin/by/youngliqui/searchservice/ElasticCommerceSearchServiceApplication.kt
@@ -0,0 +1,11 @@
+package by.youngliqui.searchservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ElasticCommerceSearchServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ElasticCommerceSearchServiceApplication>(*args)
+}
